6b12fa1a90d09ec767398d189d46d5382266ca8e
lazurite5487 <a109082032@mail.shu.edu.tw>
2023-04-05 19:42:01 +0800

    卡

---
+++ Assets/Script/attackText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackText : MonoBehaviour
+{
+	public GameObject card;
+	private Vector3 TargetRotation;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    TargetRotation.x = card.transform.position.x;
+	    TargetRotation.z = card.transform.position.z;
+	    TargetRotation.y = 0;
+	    transform.LookAt(TargetRotation);
+    }
+}
